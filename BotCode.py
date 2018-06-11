c4248a9425755650edfecc9b13852b77e8a91667
William Castelli (Billy) <billycastelli@gmail.com>
2018-06-11 10:52:02 -0700

    Update and rename RealBot.py to BotCode.py

--- BotCode.py
+++ BotCode.py
@@ -62,20 +62,4 @@ if __name__ == "__main__":
     streamBot()
 
     
-    
-    
- '''
- import praw
-import wikipedia
-
-
-reddit = praw.Reddit(client_id= "MyLR0f0xNdHiqQ",
-                     client_secret = "0HiOXaQG-F0IUIRo8HWMtGECB2E",
-                     username = "TheZotBot",
-                     password = "password",
-                     user_agent = 'web:WikiBot:1.0 (by /u/TheZotBot)')
-
-
-
- 
- '''
+   
